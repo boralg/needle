6907a955bf8027deb5a57928ac3a7a35c673412d
boralg <thinnrthinkr@gmail.com>
2025-04-20 23:40:46 +0300

    feat: set up glyphon

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use glyphon::{
+    Attrs, Buffer, Cache, Color, Family, FontSystem, Metrics, Resolution, Shaping, SwashCache,
+    TextArea, TextAtlas, TextBounds, TextRenderer, Viewport,
+};
 use sursface::app::AppState;
 use sursface::display::Display;
 use sursface::std::{clear, create_render_pipeline, create_shader, get_framebuffer};
-use sursface::wgpu::{self, Color, RenderPipeline};
+use sursface::wgpu::{
+    self, CommandEncoderDescriptor, LoadOp, MultisampleState, Operations,
+    RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline, TextureViewDescriptor,
+};
 
 fn main() {
     #[cfg(not(target_arch = "wasm32"))]
     {
-        sursface::start::create_window_desktop::<TriangleState>(1280, 720);
+        sursface::start::create_window_desktop::<State>(1280, 720);
     }
 }
 
@@ -16,49 +23,122 @@ pub fn start_browser(canvas: sursface::wgpu::web_sys::HtmlCanvasElement) {
     sursface::start::create_window_browser::<TriangleState>(canvas);
 }
 
-struct TriangleState {
-    render_pipeline: RenderPipeline,
+struct State {
+    font_system: FontSystem,
+    swash_cache: SwashCache,
+    viewport: glyphon::Viewport,
+    atlas: glyphon::TextAtlas,
+    text_renderer: glyphon::TextRenderer,
+    text_buffer: glyphon::Buffer,
 }
 
-impl AppState for TriangleState {
-    fn new(display: &mut Display) -> TriangleState {
-        let device = &display.device;
+impl AppState for State {
+    fn new(display: &mut Display) -> Self {
+        let mut font_system = FontSystem::new();
+        let swash_cache = SwashCache::new();
+        let cache = Cache::new(&display.device);
+        let viewport = Viewport::new(&display.device, &cache);
+        let mut atlas = TextAtlas::new(
+            &display.device,
+            &display.queue,
+            &cache,
+            display.config.format,
+        );
+        let text_renderer = TextRenderer::new(
+            &mut atlas,
+            &display.device,
+            MultisampleState::default(),
+            None,
+        );
+        let mut text_buffer = Buffer::new(&mut font_system, Metrics::new(30.0, 42.0));
 
-        let shader = create_shader(device, include_str!("assets/shader.wgsl"));
+        let physical_size = display.window.inner_size();
+        let scale_factor = display.window.scale_factor();
 
-        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: None,
-            bind_group_layouts: &[],
-            push_constant_ranges: &[],
-        });
+        let physical_width = (physical_size.width as f64 * scale_factor) as f32;
+        let physical_height = (physical_size.height as f64 * scale_factor) as f32;
 
-        let render_pipeline = create_render_pipeline(display, pipeline_layout, shader, &[]);
-        TriangleState { render_pipeline }
+        text_buffer.set_size(
+            &mut font_system,
+            Some(physical_width),
+            Some(physical_height),
+        );
+        text_buffer.set_text(&mut font_system, "Hello world! 👋\nThis is rendered with 🦅 glyphon 🦁\nThe text below should be partially clipped.\na b c d e f g h i j k l m n o p q r s t u v w x y z", &Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+        text_buffer.shape_until_scroll(&mut font_system, false);
+
+        Self {
+            font_system,
+            swash_cache,
+            viewport,
+            atlas,
+            text_renderer,
+            text_buffer,
+        }
     }
 
     fn draw(&mut self, display: &mut Display) {
-        let clear_color = Color {
-            r: 100.0 / 255.0,
-            g: 149.0 / 255.0,
-            b: 237.0 / 255.0,
-            a: 255.0 / 255.0,
-        };
+        self.viewport.update(
+            &display.queue,
+            Resolution {
+                width: display.config.width,
+                height: display.config.height,
+            },
+        );
 
-        let device = &display.device;
+        self.text_renderer
+            .prepare(
+                &display.device,
+                &display.queue,
+                &mut self.font_system,
+                &mut self.atlas,
+                &self.viewport,
+                [TextArea {
+                    buffer: &self.text_buffer,
+                    left: 10.0,
+                    top: 10.0,
+                    scale: 1.0,
+                    bounds: TextBounds {
+                        left: 0,
+                        top: 0,
+                        right: 600,
+                        bottom: 160,
+                    },
+                    default_color: Color::rgb(255, 255, 255),
+                    custom_glyphs: &[],
+                }],
+                &mut self.swash_cache,
+            )
+            .unwrap();
 
-        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: None,
-        });
-
-        let (output, view) = get_framebuffer(&display.surface);
+        let frame = display.surface.get_current_texture().unwrap();
+        let view = frame.texture.create_view(&TextureViewDescriptor::default());
+        let mut encoder = display
+            .device
+            .create_command_encoder(&CommandEncoderDescriptor { label: None });
         {
-            let mut rpass = clear(&view, &mut encoder, clear_color);
+            let mut pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: Operations {
+                        load: LoadOp::Clear(wgpu::Color::BLACK),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
 
-            rpass.set_pipeline(&self.render_pipeline);
-            rpass.draw(0..3, 0..1);
+            self.text_renderer
+                .render(&self.atlas, &self.viewport, &mut pass)
+                .unwrap();
         }
 
-        display.queue.submit(std::iter::once(encoder.finish()));
-        output.present();
+        display.queue.submit(Some(encoder.finish()));
+        frame.present();
+
+        self.atlas.trim();
     }
 }
